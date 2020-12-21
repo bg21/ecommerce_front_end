729285170a55d4c2bb63aed557a2f3f2bac059cd
bg21 <juhcosta23@gmail.com>
2020-12-21 15:31:26 -0300

    topo

--- index.php
+++ index.php
@@ -4,19 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <header>
             <section class="alert">
                 <div class="container">
-                    uma promoção incrível
+                    <p><i class="fas fa-tag"></i> Promoção - R$ 19,90</p>
                 </div>
             </section>
 
             <section class="topo">
                 <div class="container">
-                    <a href="">Logo</a>
+                    <div class="left">
+                        <a href=""><span>STORE</span><span>GO</span></a>
+                        <div class="search">
+                            <form action="" method="get">
+                                <input type="search" name="" id="">
+                                <button type="submit"><i class="fas fa-user"></i></button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <div class="conta">
+                            <a href=""><i class="fas fa-user"></i> Conta</a>
+                            <div>
+                                <a href="">Registrar</a>
+                            </div>
+                        </div>
+                        <div class="carrinho">
+                            <a href=""><i class="fas fa-shopping-cart"></i> Carrinho</a>
+                            <div>Itens ()</div>
+                        </div>
+                    </div>
                 </div>
             </section>
 
