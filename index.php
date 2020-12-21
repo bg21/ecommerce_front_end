8017077f050a9889c0586198c0a23028d7d8c9b6
bg21 <juhcosta23@gmail.com>
2020-12-21 15:07:43 -0300

    iniciando

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+            <section class="alert">
+                <div class="container">
+                    uma promoção incrível
+                </div>
+            </section>
+
+            <section class="topo">
+                <div class="container">
+                    <a href="">Logo</a>
+                </div>
+            </section>
+
+            <nav>
+                <div class="container">
+                    <ul>
+                        <li><a href="">home</a></li>
+                    </ul>
+                </div>
+            </nav>
+    </header>
+</body>
+</html>
